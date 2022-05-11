484f7993790c65943ce3b73f94924981e64b865b
PranjalAgrawal1 <pranjal605@gmail.com>
2022-05-11 21:55:40 +0530

    Structure of Cart

--- src/CartItem.js
+++ src/CartItem.js
@@ -1,7 +1,48 @@
 import React from "react";
 
 class CartItem extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            price: 9999,
+            name: "Phone",
+            qty: 1,
+            img: ""
+        }
+    }
+
+    increaseQuantity = () => {
+        this.setState((prevState) => {
+            return {
+                qty : prevState.qty + 1
+            }
+        });
+
+    }
+
+    decreseQuantity = () =>{
+        if(this.state.qty == 0){
+            return;
+        }
+        
+        this.setState((prevState) => {
+            return {
+                qty : prevState.qty - 1
+            }
+        });
+
+    }
+
+    deleteCartItem = () => {
+        this.setState((prevState) => {
+            return {
+                qty : 0
+            }
+        });
+    }
+
     render() {
+        const {price, name, qty} = this.state;
         return (
             <div className="cart-item" >
                 <div className="left-block">
@@ -9,10 +50,35 @@ class CartItem extends React.Component {
 
                 </div>
                 <div className="right-block">
-                    <div style={styles.name}>Phone</div>
-                    <div style={styles.quantity}>Rs 999</div>
-                    <div style={styles.quantity}>Qty: 1</div>
+                    <div style={styles.name}>{name}</div>
+                    <div style={styles.quantity}>Rs {price}</div>
+                    <div style={styles.quantity}>Qty: {qty}</div>
                     <div className="cart-item-actions">
+
+                        <img 
+                            alt="increase" 
+                            className="action-icons" 
+                            // src="https://cdn-icons.flaticon.com/png/512/2040/premium/2040520.png?token=exp=1652256168~hmac=42f14b538f9827b3ce0ed8610b0b9850" 
+                            src="./plus.png"
+                            onClick={this.increaseQuantity}
+                        />
+                        <img 
+                            alt="decrease" 
+                            className="action-icons" 
+                            // src="https://cdn-icons.flaticon.com/png/512/2040/premium/2040522.png?token=exp=1652256014~hmac=e85ee22b4d66bc31e3cb8f0f172829b4" 
+                            src="./minus.png"
+                            onClick={this.decreseQuantity}
+
+                        />
+                        <img 
+                            alt="delete" 
+                            className="action-icons" 
+                            // src="https://cdn-icons.flaticon.com/png/512/2040/premium/2040464.png?token=exp=1652256182~hmac=aa77a95ff9319e85436d84f31eaa0998" 
+                            src="./delete.png"
+                            onClick={this.deleteCartItem}
+
+                        />
+
                     </div>
                 </div>
 
@@ -22,17 +88,17 @@ class CartItem extends React.Component {
 }
 
 const styles = {
-    image : {
-        height : 110,
-        width : 110,
+    image: {
+        height: 110,
+        width: 110,
         borderRadius: 4
     },
-    name:{
-        fontSize:25
+    name: {
+        fontSize: 25
 
     },
 
-    quantity:{
+    quantity: {
         color: "#777"
     }
 
