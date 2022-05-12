ebfa5b1c3c6eae4f009dfa9f140d598cc5961e4a
PranjalAgrawal1 <pranjal605@gmail.com>
2022-05-12 20:37:02 +0530

    Total of the cart value

--- src/App.js
+++ src/App.js
@@ -1,16 +1,121 @@
 import React from "react";
-import CartItem from "./CartItem";
 import Cart from "./Cart";
+import Navbar from "./Navbar";
 
 
-function App() {
-  return (
-    <div className="App">
-      <h1> Cart </h1>
-      <Cart />
+class App extends React.Component {
 
-    </div>
-  );
+  constructor() {
+    super();
+    this.state = {
+      products: [
+        {
+          id: 1,
+          qty: 1,
+          price: 999,
+          name: "Phone",
+          img: "https://images.samsung.com/is/image/samsung/p6pim/in/2202/gallery/in-galaxy-s22-plus-s906-412931-sm-s906ezwdinu-thumb-530963903?$264_264_PNG$"
+        },
+        {
+          id: 2,
+          qty: 1,
+          price: 999,
+          name: "Laptop",
+          img: "https://static.acer.com/up/Resource/Acer/Laptops/Aspire_5/images/20200908/Acer-Aspire-5_A514-54-54G_Gold_modelmain.png"
+        },
+        {
+          id: 3,
+          qty: 1,
+          price: 999,
+          name: "Watch",
+          img: "https://cdn.shopify.com/s/files/1/0057/8938/4802/products/9da7e1ae-89f2-42c4-9714-76ac5ec83817.png?v=1625045743"
+        },
+        {
+          id: 4,
+          qty: 1,
+          price: 999,
+          name: "Shirt",
+          img: "https://dictionary.cambridge.org/images/thumb/shirt_noun_002_33400.jpg"
+        },
+      ]
+    }
+  }
+
+  handleIncreaseQuantity = (product) => {
+    // console.log("hey increase the quantity of", product);
+    const { products } = this.state;
+    const index = products.indexOf(product);
+    products[index].qty += 1;
+    this.setState({
+        products
+    })
+  }
+
+  handleDecreseQuantity = (product) => {
+    // console.log("hey decrease the quantity of", product);
+    const { products } = this.state;
+    const index = products.indexOf(product);
+    if (products[index].qty === 0) {
+        // handleDeleteProduct(products[index].id);
+        return;
+    }
+    products[index].qty -= 1;
+    this.setState({
+        products
+    })
+  }
+
+  handleDeleteProduct = (id) => {
+    const { products } = this.state;
+    const items = products.filter((item) => item.id !== id);
+    this.setState({
+      products: items
+    })
+  }
+
+  getCartCount = () => {
+    const{products} = this.state;
+    let count = 0;
+
+    products.forEach((product) => {
+      count += product.qty
+    })
+    if(count > 9){
+      count = '9+'
+    }
+    return count;
+  }
+  getCartTotal=()=>{
+    const{products} = this.state;
+    let total = 0;
+    products.map((product) => {
+      total += (product.qty * product.price)
+    })
+    return total;
+  }
+
+  render(){
+    const{products} = this.state;
+
+    return (
+      <div className="App">
+        <Navbar
+          count = {this.getCartCount()}
+        />
+        <Cart
+          products = {products}
+          onIncreaseQuantity={this.handleIncreaseQuantity}
+          onDecreseQuantity={this.handleDecreseQuantity}
+          onDeleteItem = {this.handleDeleteProduct}
+        />
+
+        <div>
+          Total - Rs {this.getCartTotal()}
+        </div>
+
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/CartItem.js
+++ src/CartItem.js
@@ -1,61 +1,45 @@
 import React from "react";
 
-class CartItem extends React.Component {
+const CartItem = (props) => {
 
 
-    // deleteCartItem = () => {
-    //     this.setState((prevState) => {
-    //         return {
-    //             qty : 0
-    //         }
-    //     });
-    // }
-
-    render() {
-        const { qty, price, name } = this.props.product;
-        const { product, onIncreaseQuantity, onDecreseQuantity, onDeleteItem} = this.props;
-        return (
-            <div className="cart-item" >
-                <div className="left-block">
-                    <img style={styles.image} />
-
-                </div>
-                <div className="right-block">
-                    <div style={styles.name}>{name}</div>
-                    <div style={styles.quantity}>Rs {price}</div>
-                    <div style={styles.quantity}>Qty: {qty}</div>
-                    <div className="cart-item-actions">
-
-                        <img
-                            alt="increase"
-                            className="action-icons"
-                            // src="https://cdn-icons.flaticon.com/png/512/2040/premium/2040520.png?token=exp=1652256168~hmac=42f14b538f9827b3ce0ed8610b0b9850" 
-                            src="./plus.png"
-                            onClick={() => onIncreaseQuantity(product)}
-                        />
-                        <img
-                            alt="decrease"
-                            className="action-icons"
-                            // src="https://cdn-icons.flaticon.com/png/512/2040/premium/2040522.png?token=exp=1652256014~hmac=e85ee22b4d66bc31e3cb8f0f172829b4" 
-                            src="./minus.png"
-                            onClick={() => onDecreseQuantity(product)}
-
-                        />
-                        <img
-                            alt="delete"
-                            className="action-icons"
-                            // src="https://cdn-icons.flaticon.com/png/512/2040/premium/2040464.png?token=exp=1652256182~hmac=aa77a95ff9319e85436d84f31eaa0998" 
-                            src="./delete.png"
-                            onClick={() => onDeleteItem(product.id)}
-
-                        />
-
-                    </div>
-                </div>
-
+    const { qty, price, name, img } = props.product;
+    const { product, onIncreaseQuantity, onDecreseQuantity, onDeleteItem} = props;
+    return (
+        <div className="cart-item" >
+            <div className="left-block">
+                <img style={styles.image} src={img}/>
             </div>
-        )
-    }
+            <div className="right-block">
+                <div style={styles.name}>{name}</div>
+                <div style={styles.quantity}>Rs {price}</div>
+                <div style={styles.quantity}>Qty: {qty}</div>
+                <div className="cart-item-actions">
+                    <img
+                        alt="increase"
+                        className="action-icons"
+                        // src="https://cdn-icons.flaticon.com/png/512/2040/premium/2040520.png?token=exp=1652256168~hmac=42f14b538f9827b3ce0ed8610b0b9850" 
+                        src="./plus.png"
+                        onClick={() => onIncreaseQuantity(product)}
+                    />
+                    <img
+                        alt="decrease"
+                        className="action-icons"
+                        // src="https://cdn-icons.flaticon.com/png/512/2040/premium/2040522.png?token=exp=1652256014~hmac=e85ee22b4d66bc31e3cb8f0f172829b4" 
+                        src="./minus.png"
+                        onClick={() => onDecreseQuantity(product)}
+                    />
+                    <img
+                        alt="delete"
+                        className="action-icons"
+                        // src="https://cdn-icons.flaticon.com/png/512/2040/premium/2040464.png?token=exp=1652256182~hmac=aa77a95ff9319e85436d84f31eaa0998" 
+                        src="./delete.png"
+                        onClick={() => onDeleteItem(product.id)}
+                    />
+                </div>
+            </div>
+        </div>
+    )
 }
 
 const styles = {
